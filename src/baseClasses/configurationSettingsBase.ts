402435fa8483b8bb45d7902f75367255abd654d2
Cameron Smith <cam11023@esri.com>
2021-01-06 14:50:56 -0800

    Update _isWithinConfigurationExperience function

--- src/baseClasses/configurationSettingsBase.ts
+++ src/baseClasses/configurationSettingsBase.ts
@@ -1,4 +1,4 @@
-import Accessor from 'esri/core/Accessor';
+import Accessor from 'esri/core/Accessor'
 import { property } from 'esri/core/accessorSupport/decorators'
 
 /**
@@ -8,47 +8,56 @@ import { property } from 'esri/core/accessorSupport/decorators'
 class ConfigurationSettingsBase extends Accessor {
   /** Determines if the App is being run within the Config Panel's IFrame */
   @property()
-  withinConfigurationExperience: boolean = this._isWithinConfigurationExperience();
+  withinConfigurationExperience: boolean = this._isWithinConfigurationExperience()
 
-  private _draft: any = null;
-  private _draftMode: boolean = false;
+  private _draft: any = null
+  private _draftMode: boolean = false
 
   constructor(params?: any) {
-    super();
-    this._draft = params?.draft;
-    this._draftMode = params?.mode === 'draft';
+    super()
+    this._draft = params?.draft
+    this._draftMode = params?.mode === 'draft'
   }
 
   initialize() {
     if (this.withinConfigurationExperience || this._draftMode) {
       // Apply any draft properties
       if (this._draft) {
-        Object.assign(this, this._draft);
+        Object.assign(this, this._draft)
       }
 
       window.addEventListener(
         'message',
         (e: any) => {
-          this._handleConfigurationUpdates(e);
+          this._handleConfigurationUpdates(e)
         },
-        false
-      );
+        false,
+      )
     }
   }
 
   _handleConfigurationUpdates(e: any) {
     if (e?.data?.type === 'cats-app') {
-      Object.assign(this, e.data);
+      Object.assign(this, e.data)
     }
   }
 
   private _isWithinConfigurationExperience(): boolean {
-    const { frameElement, location, parent } = window;
+    const { frameElement, location, parent } = window // If frameElement is null, origins between parent and child do not match
     return frameElement
-      ? frameElement.getAttribute("data-embed-type") === "instant-config"
-        ? true
-        : false
-      : location !== parent.location;
+      ? // If origins match, check if parent iframe has data-embed-type="instant-config"
+        frameElement.getAttribute('data-embed-type') === 'instant-config'
+        ? // If so, app is within config experience - use draft values
+          true
+        : // Otherwise, it is not within config experience - use publish values
+          false
+      : // Origins do not match
+        // IF TRUE - If parent and child locations do not match, and the location hostnames are local host.
+        // Use draft values for locally hosted config panel testing
+        // IF FALSE - template app is embedded on hosted page - use publish values.
+        location !== parent.location &&
+          (location.hostname === 'localhost' ||
+            location.hostname === '127.0.0.1')
   }
 }
-export = ConfigurationSettingsBase;
+export = ConfigurationSettingsBase
