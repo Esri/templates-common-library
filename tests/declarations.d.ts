ceb98fcc68019812b96624e2cee4aeb643887086
Cameron Smith <c.smith@esri.com>
2021-07-28 15:16:44 -0700

    Swapped out Jest for Intern for test runner

---
+++ tests/declarations.d.ts
@@ -0,0 +1,20 @@
+export declare var define: any
+declare module "baseClasses/*" {
+  const value: any;
+  export = value;
+}
+
+declare module "functionality/*" {
+  const value: any;
+  export = value;
+}
+
+declare module "interfaces/*" {
+  const value: any;
+  export = value;
+}
+
+declare module "structuralFunctionality/*" {
+  const value: any;
+  export = value;
+}
