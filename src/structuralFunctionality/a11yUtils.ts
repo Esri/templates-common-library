c2be8d631e28836f53e16d007411c265d718a23f
Kelly Hutchins <khutchins@esri.com>
2021-11-03 14:19:13 -0700

    add a11y utils

---
+++ src/structuralFunctionality/a11yUtils.ts
@@ -0,0 +1,40 @@
+
+
+const liveRegionId = "a11y-live-region";
+
+interface LiveRegionParams {
+    id?: string;
+    parent?: HTMLElement;
+    mode?: "polite" | "assertive";
+}
+
+export function setupLiveRegion(params?: LiveRegionParams): HTMLElement {
+    const root = params?.parent ?? document.body;
+    const regionId = params?.id ?? liveRegionId;
+    const mode = params?.mode ?? "polite";
+    let region = root.querySelector<HTMLElement>(`#${regionId}`);
+    if (!region) {
+        region = document.createElement("div");
+        region.id = regionId;
+        region.classList.add("screen-readers-only");
+        region.setAttribute("role", "alert");
+        region.setAttribute("aria-live", mode);
+        region.setAttribute("aria-atomic", "true");
+        region.textContent = "";
+        root.appendChild(region);
+    }
+    return region;
+}
+
+export function postToLiveRegion(message: string, id?: string) {
+    const regionId = id ? id : liveRegionId;
+    const region = document.body.querySelector<HTMLElement>(`#${regionId}`);
+    if (region) {
+        region.textContent = message;
+    }
+}
+
+export default {
+    setupLiveRegion,
+    postToLiveRegion
+};
