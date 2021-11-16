ca018d09a7f2971ec46cccf88f5f754d02d95a1c
ryan0000 <rlibed@esri.com>
2021-11-16 11:24:26 -0800

    Utility functions to handle tokens for images as portal item resources

---
+++ src/functionality/appAccess.ts
@@ -0,0 +1,3 @@
+export function isPublic(portalItem: __esri.PortalItem): boolean {
+  return portalItem.access === "public";
+}
---
+++ src/functionality/token.ts
@@ -0,0 +1,45 @@
+export function applyImgTokens(innerHTML: string, token: string): string {
+  const bodyEl = document.createElement("body");
+  bodyEl.innerHTML = innerHTML;
+  const imageNodes = bodyEl.getElementsByTagName("img");
+  Array.from(imageNodes).forEach((img: HTMLImageElement) => {
+    const url = new URL(img.src);
+    const params = new URLSearchParams(url.search);
+    if (
+      img.src.indexOf("instantAppsConfigPanel") !== -1 &&
+      !params.get("token")
+    ) {
+      params.append("token", token);
+      const str = params.toString();
+      const paramsString = str ? str : "";
+      const src = `${url.origin}${url.pathname}${
+        paramsString ? "?" : ""
+      }${paramsString}`;
+      img.src = src;
+    }
+  });
+  return bodyEl.innerHTML;
+}
+
+export function removeImgTokens(innerHTML: string): string {
+  const bodyEl = document.createElement("body");
+  bodyEl.innerHTML = innerHTML;
+  const imageNodes = bodyEl.getElementsByTagName("img");
+  Array.from(imageNodes).forEach((img: HTMLImageElement) => {
+    const url = new URL(img.src);
+    const params = new URLSearchParams(url.search);
+    if (
+      img.src.indexOf("instantAppsConfigPanel") !== -1 &&
+      params.get("token")
+    ) {
+      params.delete("token");
+      const str = params.toString();
+      const paramsString = str ? str : "";
+      const src = `${url.origin}${url.pathname}${
+        paramsString ? "?" : ""
+      }${paramsString}`;
+      img.src = src;
+    }
+  });
+  return bodyEl.innerHTML;
+}
