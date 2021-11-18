8018165cdeb1c4bd3e59e7d4993e4ed27017c086
ryan0000 <rlibed@esri.com>
2021-11-18 15:17:26 -0800

    a11y: prefersReducedMotion util function

--- src/functionality/a11y.ts
+++ src/functionality/a11y.ts
@@ -1,4 +1,4 @@
 export function prefersReducedMotion(): boolean {
   const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
-  return mediaQuery.matches ? false : true;
+  return mediaQuery.matches ? true : false;
 }
