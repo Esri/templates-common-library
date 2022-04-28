4e3d4baf1d3ddce93be46ed0da5417eb82445c79
ryan0000 <rlibed@esri.com>
2022-04-28 11:14:18 -0700

    Added getSharedTheme function to handle re-mapping portal shared theme values to standardized custom theme values

--- src/interfaces/commonInterfaces.ts
+++ src/interfaces/commonInterfaces.ts
@@ -1,10 +1,55 @@
 export interface esriWidgetProps extends __esri.WidgetProperties {
-    config: any;
-    view?: __esri.MapView;
-    portal?: __esri.Portal;
-    propertyName?: string;
+  config: any;
+  view?: __esri.MapView;
+  portal?: __esri.Portal;
+  propertyName?: string;
 }
 
 export interface FeatureLayerFeatureEffect extends __esri.FeatureLayer {
-    featureEffect: __esri.FeatureEffect;
+  featureEffect: __esri.FeatureEffect;
+}
+
+//----------------------------------
+// Types/Interfaces for
+// Shared Theme and CustomTheme
+//----------------------------------
+
+type IThemeType = "primary" | "secondary" | "accent";
+
+export interface IPortalPropsSharedTheme {
+  header: {
+    background: string;
+    text: string;
+  };
+  body: {
+    background: string;
+    text: string;
+    link: string;
+  };
+  button: {
+    background: string;
+    text: string;
+  };
+  logo: {
+    small: string;
+    link: string;
+  };
+}
+
+export interface ITheme {
+  type: IThemeType;
+  background: string;
+  text: string;
+}
+
+export interface ICustomTheme {
+  applyPresetTheme: boolean;
+  applySharedTheme: boolean;
+  font: string;
+  preset: "shared";
+  themes: {
+    primary?: ITheme;
+    secondary?: ITheme;
+    accent?: ITheme;
+  };
 }
