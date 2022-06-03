ed56799d92205648e7fb3e8b64e1e801a0e721b2
Kelly Hutchins <khutchins@esri.com>
2022-06-03 15:04:02 -0700

    Update promise usage

--- src/structuralFunctionality/telemetry/telemetry.ts
+++ src/structuralFunctionality/telemetry/telemetry.ts
@@ -23,9 +23,8 @@
 
 
 import Accessor from 'esri/core/Accessor';
-import {property, subclass} from 'esri/core/accessorSupport/decorators';
-import { create } from 'esri/core/promiseUtils';
-import { whenDefinedOnce} from 'esri/core/watchUtils';
+import { property, subclass } from 'esri/core/accessorSupport/decorators';
+import { whenDefinedOnce } from 'esri/core/watchUtils';
 import Alert from './Alert';
 import ArcGISTelemetry from "./telemetry.dojo.min";
 
@@ -133,7 +132,7 @@ export default class Telemetry extends Accessor {
 
     @property()
     consentAlert: Alert;
-    
+
     private _adobeTracker: any;
     private gaids = ["ga1", "ga2", "ga3"];
     private adobeIds = ["ad1"];
@@ -141,7 +140,7 @@ export default class Telemetry extends Accessor {
     private appName: string;
     private settings: TelemetrySettings;
 
-    get isConsentGiven(){
+    get isConsentGiven() {
         const storageValue = localStorage.getItem(`analytics-opt-in-${this.appName}`);
         return storageValue != null && storageValue !== "false";
     }
@@ -150,14 +149,14 @@ export default class Telemetry extends Accessor {
         super(settings);
         this.settings = settings;
     }
-    
-    initialize(){
-        
+
+    initialize() {
+
         this.runInit();
-        
-        whenDefinedOnce(this, "consentAlert", ()=>{
-            this.consentAlert.watch("state", (state: any)=>{
-                if(state === "consentGiven"){
+
+        whenDefinedOnce(this, "consentAlert", () => {
+            this.consentAlert.watch("state", (state: any) => {
+                if (state === "consentGiven") {
                     this.runInit();
                 }
             })
@@ -177,8 +176,8 @@ export default class Telemetry extends Accessor {
         //   });
     }
 
-    runInit(){
-        if(this?.settings?.config?.googleAnalyticsConsent === true && !this.isConsentGiven){
+    runInit() {
+        if (this?.settings?.config?.googleAnalyticsConsent === true && !this.isConsentGiven) {
             this._initConsentAlert();
         } else {
             this._destroyConsentAlert();
@@ -186,64 +185,64 @@ export default class Telemetry extends Accessor {
         }
     }
 
-    private _initConsentAlert(){
+    private _initConsentAlert() {
         this._destroyConsentAlert();
-        
+
         const alertContainer = document.createElement("container");
         document.body.appendChild(alertContainer);
         this.consentAlert = new Alert({
-          container: alertContainer,
-          config: this.settings.config,
-          appName: this.settings.appName
+            container: alertContainer,
+            config: this.settings.config,
+            appName: this.settings.appName
         });
     }
 
-    private _destroyConsentAlert(){
-        if(this.consentAlert != null){
+    private _destroyConsentAlert() {
+        if (this.consentAlert != null) {
             this.consentAlert.destroy();
             this.consentAlert = null;
         }
     }
 
-    private async _initTelemetry(settings){
+    private async _initTelemetry(settings) {
         const { portal } = settings;
 
-            const isGoogleEnabled: boolean = this._isGoogleEnabled(settings);
-            const isAdobeEnabled: boolean = this._isAdobeEnabled(settings);
+        const isGoogleEnabled: boolean = this._isGoogleEnabled(settings);
+        const isAdobeEnabled: boolean = this._isAdobeEnabled(settings);
 
-            await this._loadGoogleAnalytics(settings);
-            this._adobeTracker = await this._loadAdobeAnalytics(settings);
+        await this._loadGoogleAnalytics(settings);
+        this._adobeTracker = await this._loadAdobeAnalytics(settings);
 
-            const options = {
-                disabled: false,
-                portal,
-                amazon: this._getAmazonCredentials(settings),
-                google: isGoogleEnabled,
-                debug: this._getEnvironment(portal.portalHostname) === "dev" ? true : false
-            };
+        const options = {
+            disabled: false,
+            portal,
+            amazon: this._getAmazonCredentials(settings),
+            google: isGoogleEnabled,
+            debug: this._getEnvironment(portal.portalHostname) === "dev" ? true : false
+        };
 
-            let telemetry;
-            if(isAdobeEnabled){ // init wrapper
-                telemetry = new AdobeWrapper({
-                    instance: new ArcGISTelemetry(options),
-                    adobeTracker: this._adobeTracker
-                })
-            }else {
-                telemetry = new ArcGISTelemetry(options) as TelemetryInstance;
-            }
+        let telemetry;
+        if (isAdobeEnabled) { // init wrapper
+            telemetry = new AdobeWrapper({
+                instance: new ArcGISTelemetry(options),
+                adobeTracker: this._adobeTracker
+            })
+        } else {
+            telemetry = new ArcGISTelemetry(options) as TelemetryInstance;
+        }
 
-            this.instance = telemetry;
-            this.state = "ready";
+        this.instance = telemetry;
+        this.state = "ready";
     }
 
     private _isGoogleEnabled(settings: TelemetrySettings) {
         // NOTE: googleAnalyticsConsent is just the general Consent Message for all Analytics providers
         const { googleAnalytics, googleAnalyticsConsent, telemetry } = settings?.config;
-        
-        if(this._isEueiDisabled(settings?.portal)){
+
+        if (this._isEueiDisabled(settings?.portal)) {
             return false;
         }
-        
+
         // Check to see if GA is enabled
         let enabled = googleAnalytics;
         let optInKey = `analytics-opt-in-${telemetry.name}`
@@ -257,7 +256,7 @@ export default class Telemetry extends Accessor {
         // NOTE: googleAnalyticsConsent is just the general Consent Message for all Analytics providers
         const { adobeLaunchAnalytics, googleAnalyticsConsent, telemetry } = settings?.config;
 
-        if(this._isEueiDisabled(settings?.portal)){
+        if (this._isEueiDisabled(settings?.portal)) {
             return false;
         }
 
@@ -289,23 +288,23 @@ export default class Telemetry extends Accessor {
     }
     private async _loadGoogleAnalytics(settings: any) {
 
-        return create(async (resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const { googleAnalyticsKey } = settings?.config;
 
             const enableGoogle = this._isGoogleEnabled(settings);
             const scriptsExist = this._googleScriptsExist();
 
             if (!enableGoogle && scriptsExist) {
-                window[`ga-disable-${googleAnalyticsKey}`] = false; 
-                resolve();
+                window[`ga-disable-${googleAnalyticsKey}`] = false;
+                Promise.resolve();
             } else if (!enableGoogle && !scriptsExist) {
-                resolve();
-            } else if(enableGoogle && scriptsExist){
+                Promise.resolve();
+            } else if (enableGoogle && scriptsExist) {
                 window[`ga-disable-${googleAnalyticsKey}`] = true;
-                resolve(); 
-            } else if(enableGoogle && !scriptsExist){
-                if(googleAnalyticsKey == null){
-                    resolve();
+                Promise.resolve();
+            } else if (enableGoogle && !scriptsExist) {
+                if (googleAnalyticsKey == null) {
+                    Promise.resolve();
                     return;
                 }
                 const gaScript0 = document.createElement('script');
@@ -317,13 +316,13 @@ export default class Telemetry extends Accessor {
                 gaScript1.setAttribute('async', 'true');
                 gaScript1.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsKey}`);
                 gaScript1.id = this.gaids[1];
-                
+
                 const gaScript2 = document.createElement('script');
                 gaScript2.id = this.gaids[2];
                 gaScript2.innerText = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date());gtag(\'config\', \'${googleAnalyticsKey}\');`;
 
                 const head = document.getElementsByTagName("head")[0];
-                
+
                 head.appendChild(gaScript0);
                 head.appendChild(gaScript1);
                 head.appendChild(gaScript2);
@@ -342,17 +341,17 @@ export default class Telemetry extends Accessor {
                 };
                 _isLoaded();
             } else {
-                resolve();
+                Promise.resolve();
             }
         });
     }
     private async _loadAdobeAnalytics(settings: any) {
 
-        return create(async (resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
 
-            const { 
-                adobeLaunchAnalyticsReportSuiteId, 
-                adobeLaunchAnalyticsScriptTag, 
+            const {
+                adobeLaunchAnalyticsReportSuiteId,
+                adobeLaunchAnalyticsScriptTag,
             } = settings.config;
 
             const enableAdobeLaunch = this._isAdobeEnabled(settings);
@@ -360,17 +359,17 @@ export default class Telemetry extends Accessor {
 
             if (!enableAdobeLaunch && scriptsExist) {
                 this.removeALScripts();
-                resolve();
+                Promise.resolve();
             } else if (!enableAdobeLaunch && !scriptsExist) {
-                resolve();
-            } else if(enableAdobeLaunch && scriptsExist){
-                resolve();
-            } else if(enableAdobeLaunch && !scriptsExist){
-                if(adobeLaunchAnalyticsScriptTag == null || adobeLaunchAnalyticsReportSuiteId == null){
-                    resolve();
+                Promise.resolve();
+            } else if (enableAdobeLaunch && scriptsExist) {
+                Promise.resolve();
+            } else if (enableAdobeLaunch && !scriptsExist) {
+                if (adobeLaunchAnalyticsScriptTag == null || adobeLaunchAnalyticsReportSuiteId == null) {
+                    Promise.resolve();
                     return;
-                }          
-                
+                }
+
                 const adScript0 = document.createElement('script');
                 const url = `//assets.adobedtm.com/${adobeLaunchAnalyticsScriptTag}`;
                 adScript0.setAttribute('src', url);
@@ -394,7 +393,7 @@ export default class Telemetry extends Accessor {
                 };
                 _isLoaded();
             } else {
-                resolve();
+                Promise.resolve();
             }
         });
     }
@@ -428,40 +427,40 @@ export default class Telemetry extends Accessor {
             return (h === "arcgis.com" && "prod") || (h === "qaext.arcgis.com" && "qa") || "dev";
         }
     }
-    private _isEueiDisabled(portal: __esri.Portal){
+    private _isEueiDisabled(portal: __esri.Portal) {
         return portal?.eueiEnabled === false;
     }
 }
 
 /** Wrapper which adds the functionality of Adobe Launch to the TelemetryInstance */
-class AdobeWrapper implements TelemetryInstance  {
+class AdobeWrapper implements TelemetryInstance {
 
     private _wrappedTelemInstance: TelemetryInstance;
     private _adobeTracker: any;
 
-    constructor(args: any){
+    constructor(args: any) {
         this._wrappedTelemInstance = args?.instance;
         this._adobeTracker = args?.adobeTracker;
     }
 
-    logPageView: (page?: string) => void = (page?: string, options?: any)=>{
+    logPageView: (page?: string) => void = (page?: string, options?: any) => {
         if (this._adobeTracker == null) {
             console.error(`this._adobeTracker function is undefined. Something went wrong with AdobeLaunch Instantiation`)
         }
         const telemetryPayload = this.createPageView({
-                page,
-                previousPage: {},
-                options
-            },
+            page,
+            previousPage: {},
+            options
+        },
             null,
             false
         )
-        
+
         this._adobeTracker.t(telemetryPayload);
-        
+
         this._wrappedTelemInstance.logPageView(page);
     };
-    logEvent: (payload: EventPayload) => boolean = (payload: EventPayload)=>{
+    logEvent: (payload: EventPayload) => boolean = (payload: EventPayload) => {
         if (this._adobeTracker == null) {
             console.error(`this._adobeTracker function is undefined. Something went wrong with AdobeLaunch Instantiation`)
         }
@@ -470,40 +469,40 @@ class AdobeWrapper implements TelemetryInstance  {
 
         return this._wrappedTelemInstance.logEvent(telemetryPayload);
     };
-    
+
     ///////////////////////////
     // Pass through functions
     ///////////////////////////
-    logError: (payload: ErrorPayload) => void = (payload: ErrorPayload)=>{
+    logError: (payload: ErrorPayload) => void = (payload: ErrorPayload) => {
         this._wrappedTelemInstance.logError(payload);
     };
-    update: (settings: any) => void = (settings: any)=>{
+    update: (settings: any) => void = (settings: any) => {
         this._wrappedTelemInstance.update(settings);
     }
-    removeScripts: () => void = ()=>{
+    removeScripts: () => void = () => {
         this._wrappedTelemInstance.removeScripts();
     }
-    get disabled(){
+    get disabled() {
         return this._wrappedTelemInstance.disabled;
     };
-    startWorkflow: (name: string,payload?: WorkflowPayload) => any 
-        = (name: string,payload?: WorkflowPayload)=>{
+    startWorkflow: (name: string, payload?: WorkflowPayload) => any
+        = (name: string, payload?: WorkflowPayload) => {
             return this._wrappedTelemInstance.startWorkflow(name, payload);
         };
-    stepWorkflow: (name: string,step: string,payload?: WorkflowPayload) => void
-        = (name: string, step: string,payload?: WorkflowPayload)=>{
+    stepWorkflow: (name: string, step: string, payload?: WorkflowPayload) => void
+        = (name: string, step: string, payload?: WorkflowPayload) => {
             return this._wrappedTelemInstance.stepWorkflow(name, step, payload);
         };
-    cancelWorkflow: (name: string,payload?: WorkflowPayload) => void
-        = (name: string,payload?: WorkflowPayload)=>{
+    cancelWorkflow: (name: string, payload?: WorkflowPayload) => void
+        = (name: string, payload?: WorkflowPayload) => {
             return this._wrappedTelemInstance.cancelWorkflow(name, payload);
         };
-    endWorkflow: (name: string,payload?: WorkflowPayload) => void
-        = (name: string,payload?: WorkflowPayload)=>{
+    endWorkflow: (name: string, payload?: WorkflowPayload) => void
+        = (name: string, payload?: WorkflowPayload) => {
             return this._wrappedTelemInstance.endWorkflow(name, payload);
         };
     getWorkflow: (name: string) => any
-        = (name: string)=>{
+        = (name: string) => {
             return this._wrappedTelemInstance.getWorkflow(name);
         };
 
@@ -517,73 +516,73 @@ class AdobeWrapper implements TelemetryInstance  {
         const payload = this.formatPayload(page, previousPage, options, "pageView");
         let defaultMapping = {};
         if (!useCustomDimensionMapping) {
-          defaultMapping = {
-            prop1: payload.eventType,
-            prop2: payload.referrer,
-            prop3: payload.hostname,
-            prop4: payload.path,
-            prop5: payload.pageName,
-            prop6: payload.previousPageName,
-            prop7: payload.previousPageUrl,
-            prop8: payload.category,
-            prop9: payload.action,
-            prop10: payload.label,
-            prop11: payload.attribute,
-            prop12: payload.details
-          }
+            defaultMapping = {
+                prop1: payload.eventType,
+                prop2: payload.referrer,
+                prop3: payload.hostname,
+                prop4: payload.path,
+                prop5: payload.pageName,
+                prop6: payload.previousPageName,
+                prop7: payload.previousPageUrl,
+                prop8: payload.category,
+                prop9: payload.action,
+                prop10: payload.label,
+                prop11: payload.attribute,
+                prop12: payload.details
+            }
         }
         return {
-          ...defaultMapping
+            ...defaultMapping
         }
-      }
-      
+    }
+
     createEventLog(event, dimensions, useCustomDimensionMapping, previousPage) {
         const payload = this.formatPayload(null, previousPage, event, "other");
-      
+
         let defaultMapping = {};
         if (!useCustomDimensionMapping) {
-          defaultMapping = {
-            prop1: payload.eventType,
-            prop2: payload.referrer,
-            prop3: payload.hostname,
-            prop4: payload.path,
-            prop5: payload.pageName,
-            prop6: payload.previousPageName,
-            prop7: payload.previousPageUrl,
-            prop8: payload.category,
-            prop9: payload.action,
-            prop10: payload.label,
-            prop11: payload.attribute,
-            prop12: payload.details
-          }
+            defaultMapping = {
+                prop1: payload.eventType,
+                prop2: payload.referrer,
+                prop3: payload.hostname,
+                prop4: payload.path,
+                prop5: payload.pageName,
+                prop6: payload.previousPageName,
+                prop7: payload.previousPageUrl,
+                prop8: payload.category,
+                prop9: payload.action,
+                prop10: payload.label,
+                prop11: payload.attribute,
+                prop12: payload.details
+            }
         }
-      
+
         return {
-          ...defaultMapping
+            ...defaultMapping
         }
-      }
+    }
 
     formatPayload(
         page,
         previousPage,
         options,
         eventType
-      ) {
-      
+    ) {
+
         const { referrer, title } = document || {}
         const { hostname, pathname } = window && window.location ? window.location : { hostname: null, pathname: null }
-      
+
         return {
-          eventType,
-          referrer,
-          hostname,
-          path: page || pathname,
-          pageName: title,
-          previousPageUrl: previousPage?.pageUrl,
-          previousPageName: previousPage?.pageName,
-          ...options
+            eventType,
+            referrer,
+            hostname,
+            path: page || pathname,
+            pageName: title,
+            previousPageUrl: previousPage?.pageUrl,
+            previousPageName: previousPage?.pageName,
+            ...options
         }
-      }
-    
+    }
+
 }
 
