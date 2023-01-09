e1f9626349ea493d52f0aa3667c8d64bd3955903
Cameron Smith <c.smith@esri.com>
2023-01-09 16:08:10 -0500

    feat: atlas and notify configParamsUtils

--- src/baseClasses/support/configParamsUtils.ts
+++ src/baseClasses/support/configParamsUtils.ts
@@ -1,41 +1,45 @@
+import Atlas from "dojo/text!../../configParamsJSON/atlasConfigParams.json";
 import AttachmentViewer from "dojo/text!../../configParamsJSON/avConfigParams.json";
 import Basic from "dojo/text!../../configParamsJSON/basicConfigParams.json";
 import CategoryGallery from "dojo/text!../../configParamsJSON/categoryGalleryConfigParams.json";
 import Charts from "dojo/text!../../configParamsJSON/chartsConfigParams.json";
 import Countdown from "dojo/text!../../configParamsJSON/countdownConfigParams.json";
+import Exhibit from "dojo/text!../../configParamsJSON/exhibitConfigParams.json";
 import Imagery from "dojo/text!../../configParamsJSON/imageryViewerConfig.json";
+import Insets from "dojo/text!../../configParamsJSON/insetConfigParams.json";
 import InteractiveLegend from "dojo/text!../../configParamsJSON/intLegendConfigParams.json";
 import Media from "dojo/text!../../configParamsJSON/mediaConfigParams.json";
 import Minimalist from "dojo/text!../../configParamsJSON/minimalistConfigParams.json";
 import Nearby from "dojo/text!../../configParamsJSON/nearbyConfigParams.json";
+import Notify from "dojo/text!../../configParamsJSON/notifyConfigParams.json";
 import Portfolio from "dojo/text!../../configParamsJSON/portfolioConfigParams.json";
 import Sidebar from "dojo/text!../../configParamsJSON/sidebarConfigParams.json";
 import Slider from "dojo/text!../../configParamsJSON/dataSliderConfigParams.json";
 import ThreeDViewer from "dojo/text!../../configParamsJSON/3dViewerConfigParams.json";
 import ZoneLookup from "dojo/text!../../configParamsJSON/lookupConfigParams.json";
-import Exhibit from "dojo/text!../../configParamsJSON/exhibitConfigParams.json";
-import Insets from "dojo/text!../../configParamsJSON/insetConfigParams.json";
 
 import { EAppTemplateType } from "../CompatibilityChecker";
 
 const INSTANT_APPS_CONFIG_PARAMS_MAP = {
+  [EAppTemplateType.Atlas]: Atlas,
   [EAppTemplateType.AttachmentViewer]: AttachmentViewer,
   [EAppTemplateType.Basic]: Basic,
   [EAppTemplateType.CategoryGallery]: CategoryGallery,
   [EAppTemplateType.Charts]: Charts,
   [EAppTemplateType.Countdown]: Countdown,
+  [EAppTemplateType.Exhibit]: Exhibit,
   [EAppTemplateType.ImageryApp]: Imagery,
   [EAppTemplateType.InteractiveLegend]: InteractiveLegend,
+  [EAppTemplateType.Insets]: Insets,
   [EAppTemplateType.Media]: Media,
   [EAppTemplateType.Minimalist]: Minimalist,
   [EAppTemplateType.Nearby]: Nearby,
+  [EAppTemplateType.Notify]: Notify,
   [EAppTemplateType.Portfolio]: Portfolio,
   [EAppTemplateType.Sidebar]: Sidebar,
   [EAppTemplateType.Slider]: Slider,
   [EAppTemplateType.ThreeDViewer]: ThreeDViewer,
   [EAppTemplateType.ZoneLookup]: ZoneLookup,
-  [EAppTemplateType.Exhibit]: Exhibit,
-  [EAppTemplateType.Insets]: Insets
 };
 
 export function getConfigParams(template: EAppTemplateType) {
