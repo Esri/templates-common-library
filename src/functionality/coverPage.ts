908fe833a1d26f6ac6e1e9833c192b15ef466fdf
Ryan Libed <RLibed@esri.com>
2023-11-21 16:10:00 -0800

    feat: add map function for landing page

---
+++ src/functionality/coverPage.ts
@@ -0,0 +1,38 @@
+import {
+  CoverPageConfig,
+  LandingPageConfig,
+} from "../interfaces/commonInterfaces";
+
+const DEFAULT = {
+  titleText: "Title",
+  subtitleText: "Subtitle",
+  descriptionText: "Description content",
+  entryButtonText: "Enter",
+  alignment: "center",
+  backgroundType: "color",
+  textColor: "#FFFFFF",
+  entryButtonColor: "#0079C1",
+  backgroundColor: "#0079C1",
+  iconImage: null,
+  iconImageScale: "m",
+  backgroundImageSrc: null,
+};
+
+export function getLandingPageValues(
+  landingPageConfig: LandingPageConfig,
+  coverPageConfig: CoverPageConfig
+): LandingPageConfig {
+  return landingPageConfig
+    ? landingPageConfig
+    : coverPageConfig
+    ? {
+        ...DEFAULT,
+        titleText: coverPageConfig?.title,
+        subtitleText: coverPageConfig?.subtitle,
+        entryButtonText: coverPageConfig?.buttonText,
+        backgroundType: coverPageConfig?.background?.backgroundType,
+        backgroundColor: coverPageConfig?.background?.backgroundColor,
+        backgroundImageSrc: coverPageConfig?.background?.backgroundImage?.url,
+      }
+    : DEFAULT;
+}
