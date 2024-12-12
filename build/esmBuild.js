ceb7ac341920fa620a5fb4851e0b3ee83af85b92
Praveen Ponnusamy <pponnusamy@esri.com>
2024-12-12 13:05:57 +0530

    esmBuild: Add JS extension to imported module paths
    To support buildless local development workflows, import statements in
    ES modules need to have .js file extension so that browsers can natively
    load them.
    
    Issue: https://devtopia.esri.com/WebGIS/arcgis-portal-app-templates/issues/11222

--- build/esmBuild.js
+++ build/esmBuild.js
@@ -17,8 +17,21 @@ const updateFiles = (files) => {
       .then(fileTxt => {
         // esm uses @arcgis/core package for JS API and the references are different
         let updatedFile = fileTxt.replaceAll(/esri\//g, "@arcgis/core/");
+
         // the previous line messes up "@esri/" imports. revert those back specifically
         updatedFile = updatedFile.replaceAll("@@arcgis/core/", "@esri/");
+
+        // Add .js extension to module specifiers that contain bare module names without file extension
+        // Ex:
+        // "@arcgis/core/core/promiseUtils" -> "@arcgis/core/core/promiseUtils.js"
+        updatedFile = updatedFile.replaceAll(/(from)\s+\"([^"\.]+)\";/g, "$1 \"$2.js\";");
+
+        // Add .js extension to module specifiers that contain a relative path without file extension
+        // Ex:
+        // "./support/configParser" -> "./support/configParser.js"
+        // "../functionality/configurationSettings" -> "../functionality/configurationSettings.js"
+        updatedFile = updatedFile.replaceAll(/(from)\s+\"(((\.\/)|(\.\.\/)+)([^"\.]+))\";/g, "$1 \"$2.js\";");
+
         // esm needs different telemetry file
         updatedFile = updatedFile.replaceAll("\"./telemetry.dojo.min\"", "\"./telemetry.min\"");
 
