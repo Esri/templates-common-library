a88654a8825b6c6023428a3d3491d480b7273a10
bra11311 <bra11311@esri.com>
2025-03-24 16:28:59 -0700

    chore: readd esmBuild.js to add JS extension to imported module paths

---
+++ build/esmBuild.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fse = require("fs-extra");
+const fs  = require("fs");
+const { map }  = require("ramda");
+
+/** Recursively get all files within a directory */
+const readDirR = (dir) => {
+  const recur = map(f => readDirR(path.join(dir, f)));
+  return fs.statSync(dir).isDirectory()
+    ? Array.prototype.concat(...recur(fs.readdirSync(dir)))
+    : dir;
+};
+
+const updateFiles = (files) => {
+  const updateFiles = map(filePath => {
+    return fse.readFile(filePath, "utf-8")
+      .then(fileTxt => {
+        // Add .js extension to module specifiers that contain bare module names without file extension
+        // Ex:
+        // "@arcgis/core/core/promiseUtils" -> "@arcgis/core/core/promiseUtils.js"
+        let updatedFile = fileTxt.replaceAll(/(from)\s+\"([^"\.]+)\";/g, "$1 \"$2.js\";");
+
+        // Add .js extension to module specifiers that contain a relative path without file extension
+        // Ex:
+        // "./support/configParser" -> "./support/configParser.js"
+        // "../functionality/configurationSettings" -> "../functionality/configurationSettings.js"
+        updatedFile = updatedFile.replaceAll(/(from)\s+\"(((\.\/)|(\.\.\/)+)([^"\.]+))\";/g, "$1 \"$2.js\";");
+                
+        return fse.writeFile(filePath, updatedFile);
+      });
+  });
+
+  return Promise.all(updateFiles(files));
+};
+
+const files = readDirR("./dist"); 
+updateFiles(files).then(()=>{
+  console.log("AMD string replacement finished");
+});
