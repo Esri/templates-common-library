7858498d0073156652ad760fd233f9f373f45504
Ryan Libed <RLibed@esri.com>
2025-04-07 13:43:29 -0700

    chore: integrate web editor into IA ecosystem

--- src/baseClasses/CompatibilityChecker.ts
+++ src/baseClasses/CompatibilityChecker.ts
@@ -31,6 +31,7 @@ export enum EAppTemplateType {
   Sidebar = "/apps/instant/sidebar/index.html",
   Slider = "/apps/instant/slider/index.html",
   Streamflow = "/apps/instant/streamflowviewer/index.html",
+  WebEditor = "/apps/webeditor/index.html",
   ZoneLookup = "/apps/instant/lookup/index.html",
 }
 
