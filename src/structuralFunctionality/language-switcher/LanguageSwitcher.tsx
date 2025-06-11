f96338169a83711a7e9257be67eac54731838f7e
Ryan Libed <RLibed@esri.com>
2025-06-11 13:50:07 -0700

    fix(language-switcher): add directives to enable compatibility with AV

--- src/structuralFunctionality/language-switcher/LanguageSwitcher.tsx
+++ src/structuralFunctionality/language-switcher/LanguageSwitcher.tsx
@@ -1,15 +1,5 @@
-/*
-  Copyright 2024 Esri
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.​
-*/
+/** @jsxRuntime classic */
+/** @jsx tsx */
 
 import {
   property,
@@ -186,7 +176,7 @@ export default class LanguageSwitcher extends Widget {
     const locales = config?.locales;
     const defaultLocale = config?.defaultLocale ?? null;
     return (
-      <div id={NODE_ID}>
+      <div>
         <instant-apps-language-switcher
           bind={this}
           afterCreate={storeNode}
